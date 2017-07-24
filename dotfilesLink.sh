ca78417634acfe28e80525b3bd004c231be257c0
peche feuille <pechefeuille@peche-book-pro.local>
2017-07-24 14:46:20 +0900

    Add .ideavimrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/Workspaces/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/Workspaces/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Workspaces/dotfiles/.vimrc ~/.ideavimrc
 ln -sf ~/Workspaces/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Workspaces/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/Workspaces/dotfiles/.gitignore-global ~/.gitignore-global
